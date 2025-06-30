a9512f834df15752d225fd88b34211e5f7559de7
zupraii <nkemene90@gmail.com>
2025-06-30 01:13:25 -0600

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,8 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
   const audio = document.createElement("audio");
-  audio.src = "https://raw.githubusercontent.com/zupraii/DOULE-HOOD-PORTAL/main/Destroy%20Lonely%20-%20If%20looks%20could%20kill%20x%20Red%20X%20'I%20think%20it%20looks%20much%20cooler%20on%20me'.mp3";
+  audio.src = "https://raw.githubusercontent.com/zupraii/DOULE-HOOD-PORTAL/main/destroy-lonely.mp3";
   audio.autoplay = true;
   audio.loop = true;
   audio.hidden = true;
   document.body.appendChild(audio);
+
+  // Para forzar reproducción después de interacción (por si autoplay falla)
+  document.body.addEventListener("click", () => {
+    audio.play().catch(() => {
+      console.log("Es necesario hacer clic para reproducir el audio");
+    });
+  });
 });
